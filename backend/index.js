3c8585d955e60f61b42ea37c40ed4665e91705e3
Siddharth Srivastava <120250296+SiddharthxsrivastavA@users.noreply.github.com>
2024-07-12 05:32:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,17 @@ const rootRouter = require("./routes/index");
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"},
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
+
 app.use(express.json());
+
+mongoose.connect()
 
 app.use("/api/v1", rootRouter);
 
