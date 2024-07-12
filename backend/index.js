c54cb009451e25247636f49985b37c64e283059a
Siddharth Srivastava <120250296+SiddharthxsrivastavA@users.noreply.github.com>
2024-07-12 05:43:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,17 +5,8 @@ const rootRouter = require("./routes/index");
 
 const app = express();
 
-app.use(cors(
-  {
-    origin: ["https://deploy-mern-1whq.vercel.app"},
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-));
-
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://sidpay:sidpayy@cluster0.3dk1n.mongodb.net/paytm");
 
 app.use("/api/v1", rootRouter);
 
